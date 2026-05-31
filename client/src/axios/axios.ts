079c293623f477308787b0811074793d858689a9
fahad rahman ch <ff865880@gmail.com>
2026-05-31 23:16:05 +0530

    feat: connect frontend to production backend

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = "http://localhost:9000";
+const API_BASE = "https://adhaar-ocr-z0lw.onrender.com";
 
 export const api = axios.create({
     baseURL: API_BASE,
